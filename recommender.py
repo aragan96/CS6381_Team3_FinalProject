9e3344ee582b5c4c3958359f673032fa5d81e9ea
Danny McCormick <dannymccormick3@gmail.com>
2018-04-27 10:05:21 -0500

    Trying to fix error with spark contexts

--- recommender.py
+++ recommender.py
@@ -4,15 +4,9 @@ import time
 
 # Code draws heavily from example code at https://github.com/apache/spark/blob/master/examples/src/main/python/mllib/recommendation_example.py
 
-def build_collaborative_filtering_model(file_location, numIterations = 10, regularization_parameter = 0.1):
+def build_collaborative_filtering_model(ratings, file_location, numIterations = 10, regularization_parameter = 0.1):
 	# file_location: Location of a file containing dataset formatted as 
 	#				 User ID, User Item, rating
-
-	# Format data
-	context = SparkContext(appName="collaborative_filtering_model")
-	data = context.textFile(file_location)
-	instances = data.map(lambda instance: instance.split(","))
-	ratings = instances.map(lambda arr: Rating(int(arr[0]), int(arr[1]), float(arr[2])))
 
 	# Build model
 	model = ALS.train(ratings, 10, numIterations, regularization_parameter)
@@ -23,6 +17,12 @@ def build_collaborative_filtering_model(file_location, numIterations = 10, regul
 	return MSE
 
 def run_collaborative_filtering_with_variable_hyperparameters(file_location):
+	# Format data
+	context = SparkContext(appName="collaborative_filtering_model")
+	data = context.textFile(file_location)
+	instances = data.map(lambda instance: instance.split(","))
+	ratings = instances.map(lambda arr: Rating(int(arr[0]), int(arr[1]), float(arr[2])))
+
 	iterations_to_test = [1, 5, 10, 20, 50]
 	regularization_parameters_to_test = [0.01, 0.1, 1, 10]
 	for iterations in iterations_to_test:
